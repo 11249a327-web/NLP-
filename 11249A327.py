c6af37d9084c4a89f24e3780c1af1a23a8384d2d
11249a327-web <11249a327@kanchiuniv.ac.in>
2026-08-19 14:47:01 +0530

    Add files via upload

---
+++ 11249A327.py
@@ -0,0 +1,8 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+input="this is nlp lab. regno is 11249a327"
+print(sent_tokenize(input))
+print(word_tokenize(input))
+for i in sent_tokenize(input):
+    print(i)
+    i in sent_tokenize(input)
+    print(i)
---
+++ porterstemmer.py
@@ -0,0 +1,64 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/scsvm/AppData/Local/Programs/Python/Python310/partofspeech.py
+[nltk_data] Downloading package punkt to
+[nltk_data]     C:\Users\scsvm\AppData\Roaming\nltk_data...
+[nltk_data]   Package punkt is already up-to-date!
+[nltk_data] Error loading average_perceptron_tagger: Package
+[nltk_data]     'average_perceptron_tagger' not found in index
+enter a sentence:i am akshaya
+
+Word	\POS_Tag
+------------------------------
+i		NN
+am		VBP
+akshaya		NN
+
+= RESTART: C:/Users/scsvm/AppData/Local/Programs/Python/Python310/partofspeech.py
+[nltk_data] Downloading package punkt to
+[nltk_data]     C:\Users\scsvm\AppData\Roaming\nltk_data...
+[nltk_data]   Package punkt is already up-to-date!
+[nltk_data] Error loading average_perceptron_tagger: Package
+[nltk_data]     'average_perceptron_tagger' not found in index
+enter a sentence:she is reading stories
+
+Word	\POS_Tag
+------------------------------
+she		PRP
+is		VBZ
+reading		VBG
+stories		NNS
+
+= RESTART: C:/Users/scsvm/AppData/Local/Programs/Python/Python310/partofspeech.py
+enter a sentence:she is studying NLP
+
+Word	\POS_Tag
+------------------------------
+she		PRP
+is		VBZ
+studying		VBG
+NLP		NNP
+
+= RESTART: C:/Users/scsvm/AppData/Local/Programs/Python/Python310/stemming.py
+is
+thi
+veri
+import
+to
+be
+pythonli
+while
+you
+are
+python
+with
+python
+.
+all
+python
+have
+poorli
+python
+atleast
+onc
