25c5c09ce1bc8c13abc845eafa8bdd6024bbfa87
11249a327-web <11249a327@kanchiuniv.ac.in>
2026-08-19 15:35:54 +0530

    Delete 11249A327.py

--- 11249A327.py
+++
@@ -1,8 +0,0 @@
-from nltk.tokenize import sent_tokenize, word_tokenize
-input="this is nlp lab. regno is 11249a327"
-print(sent_tokenize(input))
-print(word_tokenize(input))
-for i in sent_tokenize(input):
-    print(i)
-    i in sent_tokenize(input)
-    print(i)
